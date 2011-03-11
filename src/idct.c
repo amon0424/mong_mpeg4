bdea14b0d0b8771da141535c1d3500dd3a3d2c0e
mong <among0424@gmail.com>
2011-03-11 15:05:28 +0800

    idct integer optimization

--- src/idct.c
+++ src/idct.c
@@ -1,31 +1,38 @@
 #include <string.h>
 #include <math.h>
+#define SHIFT 12
+#define MULTIPLIER 4096
+#define HALF_MULTIPLIER 2048
+static int c0, c1, c2, c3, c4, c5, c6, c7;
 
-static double c0, c1, c2, c3, c4, c5, c6, c7;
-
-static double c0 = 0.7071068;
-static double c1 = 0.4903926;
-static double c2 = 0.4619398;
-static double c3 = 0.4157348;
-static double c4 = 0.3535534;
-static double c5 = 0.2777851;
-static double c6 = 0.1913417;
-static double c7 = 0.0975452;
+static int c0 = 0.7071068 * MULTIPLIER;
+static int c1 = 0.4903926 * MULTIPLIER;
+static int c2 = 0.4619398 * MULTIPLIER;
+static int c3 = 0.4157348 * MULTIPLIER;
+static int c4 = 0.3535534 * MULTIPLIER;
+static int c5 = 0.2777851 * MULTIPLIER;
+static int c6 = 0.1913417 * MULTIPLIER;
+static int c7 = 0.0975452 * MULTIPLIER;
 
 void
-idct(short *block)
+idct(short *block1)
 {
     short  j1, i, j;
-    double tmp[8];
-    double yy0, yy1, yy2, yy3, yy5, yy6;
-    double e, f, g, h, coeff[8][8];
-    short  v;
+    //double tmp[8];
+	int tmp[8];
+
+    int yy0, yy1, yy2, yy3, yy5, yy6;
+    int e, f, g, h, coeff[8][8];
+    int  v;
+	int block[64];
 
     for (i = 0; i < 8; i++)
     {
         for (j = 0; j < 8; j++)
         {
-            coeff[i][j] = (double) block[i * 8 + j];
+            //coeff[i][j] = (double) block[i * 8 + j];
+			block[i * 8 + j] = block1[i * 8 + j];
+			coeff[i][j] = block[i * 8 + j];
         }
     }
 
@@ -49,8 +56,8 @@ idct(short *block)
         yy5 = e - f;
         yy6 = h - g;
         tmp[7] = h + g;
-        tmp[5] = (yy6 - yy5) * c0;
-        tmp[6] = (yy6 + yy5) * c0;
+        tmp[5] = ((yy6 - yy5) * c0) >> SHIFT;
+        tmp[6] = ((yy6 + yy5) * c0) >> SHIFT;
         tmp[0] = yy0 + yy3;
         tmp[1] = yy1 + yy2;
         tmp[2] = yy1 - yy2;
@@ -58,8 +65,8 @@ idct(short *block)
         for (j = 0; j < 4; j++)
         {
             j1 = 7 - j;
-            block[(i<<3)+j] = tmp[j] + tmp[j1];
-            block[(i<<3)+j1] = tmp[j] - tmp[j1];
+            block[(i<<3)+j] = (tmp[j] + tmp[j1]) >> SHIFT;
+            block[(i<<3)+j1] = (tmp[j] - tmp[j1]) >> SHIFT;
         }
     }
 
@@ -83,8 +90,8 @@ idct(short *block)
         yy5 = e - f;
         yy6 = h - g;
         tmp[7] = h + g;
-        tmp[5] = (yy6 - yy5) * c0;
-        tmp[6] = (yy6 + yy5) * c0;
+        tmp[5] = ((yy6 - yy5) * c0) >> SHIFT;
+        tmp[6] = ((yy6 + yy5) * c0) >> SHIFT;
         tmp[0] = yy0 + yy3;
         tmp[1] = yy1 + yy2;
         tmp[2] = yy1 - yy2;
@@ -92,14 +99,15 @@ idct(short *block)
         for (j = 0; j < 4; j++)
         {
             j1 = 7 - j;
-            block[(j<<3)+i] = tmp[j] + tmp[j1];
-            block[(j1<<3)+i] = tmp[j] - tmp[j1];
+            block[(j<<3)+i] = (tmp[j] + tmp[j1]);
+            block[(j1<<3)+i] = (tmp[j] - tmp[j1]);
         }
     }
 
     for (i = 0; i < 64; i++)
     {
-        v = (short) floor(block[i] + 0.5);
+		v = (block[i] + HALF_MULTIPLIER) >> SHIFT;
         block[i] = (v < -256) ? -256 : ((v > 255) ? 255 : v);
+		block1[i] = block[i];
 	}
 }
