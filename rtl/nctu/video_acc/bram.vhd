a9b0c8eb2fc17c78be142b2ef78c98a5a16e17e3
mong <among0424@gmail.com>
2011-05-17 01:40:20 +0800

    Modify bram to dual port

--- rtl/nctu/video_acc/bram.vhd
+++ rtl/nctu/video_acc/bram.vhd
@@ -20,29 +20,29 @@ end BRAM;
 
 architecture Behavioral of BRAM is
 	type RAM is array (0 to 63) of std_logic_vector(15 downto 0);
-	signal DataMEM: RAM; -- no initial values
+	shared variable DataMEM: RAM; -- no initial values
 begin
 	process(CLK1)
 	begin
 		if CLK1'event and CLK1 = '1' then
 			if WE1 = '1' then
 				-- Synchronous Write
-				DataMEM(to_integer(unsigned(Addr1))) <= Data_In1;
+				DataMEM(to_integer(unsigned(Addr1))) := Data_In1;
 			end if;
 			-- Synchronous Read
 			Data_Out1 <= DataMEM(to_integer(unsigned(Addr1)));
 		end if;
 	end process;
 	
-	-- process(CLK2)
-	-- begin
-		-- if CLK2'event and CLK2 = '1' then
-			-- if WE2 = '1' then
-				-- -- Synchronous Write
-				-- DataMEM(to_integer(unsigned(Addr2))) <= Data_In2;
-			-- end if;
-			-- -- Synchronous Read
-			-- Data_Out2 <= DataMEM(to_integer(unsigned(Addr2)));
-		-- end if;
-	-- end process;
+	process(CLK2)
+	begin
+		if CLK2'event and CLK2 = '1' then
+			if WE2 = '1' then
+				-- Synchronous Write
+				DataMEM(to_integer(unsigned(Addr2))) := Data_In2;
+			end if;
+			-- Synchronous Read
+			Data_Out2 <= DataMEM(to_integer(unsigned(Addr2)));
+		end if;
+	end process;
 end Behavioral;
