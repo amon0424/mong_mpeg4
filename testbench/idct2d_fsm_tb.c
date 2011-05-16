8bf9aec26f13e93e7fda1e63cd0154ac939b8afa
mong <among0424@gmail.com>
2011-05-17 01:42:58 +0800

    Modify idct2d_fsm_tb.c

--- testbench/idct2d_fsm_tb.c
+++ testbench/idct2d_fsm_tb.c
@@ -1,16 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 volatile int *F_array  = (int *) 0xb0100000;
 volatile int *action  = (int *) 0xb0100080;
+
+short block[] = {
+    1036,    19,     5,  -147,   127,    24,    85,   -61
+};
 
 int
 main(int argc, char **argv)
 {
-	short farray[8] = { 1,2,3,4,5,6,7,8 };
+	memcpy((void *) F_array, (void *) block, sizeof(short)<<3);
 
-	//F_array[0] = *((long*)(farray[0]));
-	//F_array[1] = *((long*)(farray[2]));
-	//F_array[2] = *((long*)(farray[4]));
-	//F_array[3] = *((long*)(farray[6]));
-	F_array[0] = 0x20001;
 	*action=1;
 	while(*action);
+
+	*((long*)(block[0])) = F_array[0];
+	*((long*)(block[2])) = F_array[1];
+	*((long*)(block[4])) = F_array[2];
+	*((long*)(block[6])) = F_array[3];
+	printf("%d,%d,%d,%d", block[0], block[1], block[2], block[3]);
 }
